f278d7f277929edb3d465b97cd4ba8bebbd9b616
Dominik Hanke <mail@dominik-hanke.de>
2018-08-27 15:08:41 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    // Define the entry points of our application (can be multiple for different sections of a website)
+    entry: {
+        'main': './src/index.js',
+        'style': './src/style.js'
+    },
+
+    // Define the destination directory and filenames of compiled resources 
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    // Define development options
+    devtool: "source-map",
+
+    // Define loaders
+    module: {
+        rules: [
+            // Compile and extract SCSS files
+            {
+                test: /\.scss$/,
+                exclude: /(node_modules)/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        'loader': 'css-loader',
+                        'options': {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer')({ browsers: 'last 3 versions' })
+                            ]
+                        }
+                    },
+                    {
+                        'loader': 'sass-loader',
+                        'options': {
+                            sourceMap: true,
+                            outputStyle: this.mode === 'production' ? 'compressed' : 'expanded',
+                        }
+                    }
+                ]
+            },
+            // Use babel for JS files
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
+    },
+
+    // Define used plugins
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+};
