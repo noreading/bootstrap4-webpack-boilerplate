63651fe79e97dac37870025654a39aefa076f311
Dominik Hanke <mail@dominik-hanke.de>
2018-08-29 10:15:11 +0200

    Small fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = (env, argv) => {
+  return {
     // Define the entry points of our application (can be multiple for different sections of a website)
     entry: {
-        'main': './src/index.js',
-        'style': './src/style.js'
+      main: "./src/index.js",
+      style: "./src/style.js"
     },
 
-    // Define the destination directory and filenames of compiled resources 
+    // Define the destination directory and filenames of compiled resources
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist")
     },
 
     // Define development options
@@ -20,60 +21,62 @@ module.exports = {
 
     // Define loaders
     module: {
-        rules: [
-            // Compile and extract SCSS files
+      rules: [
+        // Compile and extract SCSS files
+        {
+          test: /\.scss$/,
+          exclude: /(node_modules)/,
+          use: [
+            MiniCssExtractPlugin.loader,
             {
-                test: /\.scss$/,
-                exclude: /(node_modules)/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        'loader': 'css-loader',
-                        'options': {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require('autoprefixer')({ browsers: 'last 3 versions' })
-                            ]
-                        }
-                    },
-                    {
-                        'loader': 'sass-loader',
-                        'options': {
-                            sourceMap: true,
-                            outputStyle: this.mode === 'production' ? 'compressed' : 'expanded',
-                        }
-                    }
-                ]
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
             },
-            // Use babel for JS files
             {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: [
+                  require("autoprefixer")({ browsers: "last 3 versions" })
+                ]
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                outputStyle:
+                  argv.mode === "production" ? "compressed" : "expanded"
+              }
             }
-        ]
+          ]
+        },
+        // Use babel for JS files
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["babel-preset-env"]
+            }
+          }
+        }
+      ]
     },
 
     // Define used plugins
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        })
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      })
     ]
+  };
 };
