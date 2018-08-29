515cf2b70319b4442650e1434c928a10d11ff1d8
Dominik Hanke <mail@dominik-hanke.de>
2018-08-29 13:25:25 +0200

    Updated to new babel version

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = (env, argv) => {
           use: {
             loader: "babel-loader",
             options: {
-              presets: ["babel-preset-env"]
+              presets: ["@babel/preset-env"]
             }
           }
         }
